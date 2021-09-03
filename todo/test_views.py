9f7857fd09b3b60fc4bcf0ca60b284062436b791
craigmantona <craigmantona@hotmail.com>
2021-09-03 14:45:59 +0000

    A bit late, but first commit.

---
+++ todo/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+
+# Create your tests here.
+class TestViews(TestCase):
+
+    def test_get_to_do_list(self):
+
+    def test_get_add_item_page(self):
+    
+    def test_get_edit_item_page(self):
+
+    def test_can_add_item(self):
+
+    def test_can_delete_item(self):
+
+    def test_can_toggle_item(self):
+
